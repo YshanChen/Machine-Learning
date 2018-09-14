490ff5f0c9eb119ce01a8f00fc50487a5d85ab73
YshanChen <eason.yshan@gmail.com>
2018-09-14 15:46:00 +0800

    # 统一 ID3 & C4.5算法

--- Algorithms/Decision Tree/Decision Tree.py
+++ Algorithms/Decision Tree/Decision Tree.py
@@ -10,6 +10,8 @@ import time
 '''
 统一决策树算法框架
 
+delta: 即sklearn库中的min_impurity_split, 节点划分最小不纯度，如果不纯度小于该值则停止分裂。
+
 '''
 
 class DTree(object):
@@ -30,10 +32,7 @@ class DTree(object):
             self.DTree = C4_5(X=X, y=y, method=self.method, delta=self.params['delta'])
 
     def predict(self, new_data):
-        if self.method == 'ID3':
-            return ID3_predict(DTree=self.DTree, new_data=new_data)
-        if self.method == 'C4.5':
-            return C45_predict(DTree=self.DTree, new_data=new_data)
+        return predict(DTree=self.DTree, new_data=new_data)
 
 # 信息增益算法 -----------------------------------------------------
 # 特征分裂向量 （计算每个特征的每个取值对应的Y类别的个数）
@@ -74,7 +73,7 @@ def feature_split(data, y):
     return feature_split_dic
 
 
-# 数据集D的经验熵（empirical entropy）
+# 数据集D的经验熵
 def entropy(Di_dic):
     # Di_dic => np.array
     if isinstance(Di_dic, dict):
@@ -184,10 +183,11 @@ def gain_max(data, y, method):
 
 
 # 训练 ---------------------------------------------------------
-def Decision_Tree(DTree, y, delta):
+def Decision_Tree(DTree, y, method, delta):
     for key, value in DTree.items():
+        print(key)
         # key = key
-        # value = value
+        # value = value value = DTree[key]
 
         # 子树
         subTree = {}
@@ -200,25 +200,23 @@ def Decision_Tree(DTree, y, delta):
             X = data.drop([y], axis=1).columns
 
             # 判断：信息增益是否达到阈值 & 是否特征变量>=1
-            if len(X) >= 1 and gain_max(data, y)[1] >= delta:
-                split_feature_name = gain_max(data, y)[0]
+            gain_list = gain_max(data, y, method)
+            if len(X) > 1 and gain_list[1] >= delta:
+                split_feature_name = gain_list[0]
 
                 for cat in data[split_feature_name].cat.categories:
 
-                    # 分裂
+                    # 分裂  cat = 0
                     df_split_temp = data[data[split_feature_name] == cat].drop(split_feature_name,axis=1)
                     description = ' '.join([str(split_feature_name),'=',str(cat)])
 
                     # 停止条件判断： 分裂后类别是否唯一 & 分裂后是否为空置
                     if (len(df_split_temp[y].unique()) != 1) and (df_split_temp.empty != True):
-
                         currentTree = {description: df_split_temp}
-                        currentValue = Decision_Tree(currentTree, y, delta) # 递归
-
+                        currentValue = Decision_Tree(currentTree, y, method, delta) # 递归
                         subTree.update(currentValue)
 
                     else:
-
                         # 分裂后类别唯一，叶子结点为该类别 (需要分裂)
                         if (len(df_split_temp[y].unique()) == 1):
                             leaf_node = df_split_temp[y].values[0]
@@ -229,13 +227,15 @@ def Decision_Tree(DTree, y, delta):
 
                         subTree.update({description: leaf_node})
 
-            # 停止条件判断：特征变量<1，取分裂前样本最多的类别 (不分裂)
-            elif len(X) < 1:
+            # 停止条件判断：特征变量<=1，取样本最多的类别 (不分裂)
+            elif len(X) <= 1:
+                print('特征变量<=1')
                 leaf_node = data[y].value_counts().idxmax()
                 subTree = leaf_node
 
             # 停止条件判断：分裂后最大信息增益小于阈值，取分裂前样本最多的类别 (不分裂)
-            elif gain_max(data,y)[1] < delta:
+            elif gain_max(data, y, method)[1] < delta:
+                print('分裂后最大信息增益小于阈值')
                 leaf_node = data[y].value_counts().idxmax() # 分裂前的最多类别
                 subTree = leaf_node
 
@@ -246,7 +246,7 @@ def Decision_Tree(DTree, y, delta):
 
     return DTree
 
-def ID3(X=X, y=y, method=method, delta=0.005):
+def ID3(X, y, method, delta=0.005):
     # Data
     data = pd.concat([X, y], axis=1).rename(str, columns={y.name:'label'})
 
@@ -288,9 +288,9 @@ def ID3(X=X, y=y, method=method, delta=0.005):
             currentTree = {description: currentValue}
             DTree.update(currentTree)
 
-    return Decision_Tree(DTree, y, delta)
+    return Decision_Tree(DTree=DTree, y=y, method=method, delta=delta)
 
-def C4_5(X=X, y=y, method=method, delta=0.005):
+def C4_5(X, y, method, delta=0.005):
     # Data
     data = pd.concat([X, y], axis=1).rename(str, columns={y.name:'label'})
 
@@ -302,8 +302,9 @@ def C4_5(X=X, y=y, method=method, delta=0.005):
 
     DTree = {}
 
-    if gain_max(data, y, method)[1] >= delta:
-        split_feature_name = gain_max(data, y)[0]
+    gain_list = gain_max(data, y, method)
+    if gain_list[1] >= delta:
+        split_feature_name = gain_list[0]
 
         # 初次分裂
         for cat in data[split_feature_name].cat.categories:
@@ -316,15 +317,15 @@ def C4_5(X=X, y=y, method=method, delta=0.005):
             # 分裂后数据集
             currentValue = data_split_temp
 
-            # 停止分裂判断：如果分裂后最大信息增益依然小于delta，则停止分裂，叶子结点为当前数据集下样本最多的类别
-            if gain_max(data_split_temp, y)[1] < delta:
+            # 停止分裂判断 (1)：如果分裂后最大信息增益依然小于delta，则停止分裂，叶子结点为当前数据集下样本最多的类别
+            if gain_max(data_split_temp, y, method)[1] < delta:
                 currentValue = data_split_temp[y].value_counts().idxmax()
 
-            # 停止分裂判断：如果分裂后类别唯一，则停止分裂，叶子结点为该类别
-            if (len(data_split_temp[y].unique()) == 1):
+            # 停止分裂判断 (2)：如果分裂后类别唯一，则停止分裂，叶子结点为该类别
+            if len(data_split_temp[y].unique()) == 1:
                 currentValue = data_split_temp[y].unique()[0]
 
-            # 停止分裂判断：如果分裂后为空集，则停止分裂，叶子结点为分裂前的最多类别
+            # 停止分裂判断 (3)：如果分裂后为空集，则停止分裂，叶子结点为分裂前的最多类别
             if data_split_temp.empty == True:
                 currentValue = data[y].value_counts().idxmax() # 分裂前的最多类别
 
@@ -332,7 +333,7 @@ def C4_5(X=X, y=y, method=method, delta=0.005):
             currentTree = {description: currentValue}
             DTree.update(currentTree)
 
-    return Decision_Tree(DTree, y, delta)
+    return Decision_Tree(DTree=DTree, y=y, method=method, delta=delta)
 
 
 # 预测 ---------------------------------------------------------
@@ -346,7 +347,7 @@ def most_leaf_node(tree, leaf_node_list):
     return leaf_node_list
     # return max(set(leaf_node_list), key=leaf_node_list.count)
 
-def ID3_predict_one(DTree, row_data):
+def predict_one_by_one(DTree, row_data):
     for keys,values in DTree.items():
         T_key = keys
         T_value = values
@@ -359,11 +360,11 @@ def ID3_predict_one(DTree, row_data):
         # ID3 非二叉树
         if str(row_data[split_feature]) == split_feature_value: # 符合就继续往下走
             if isinstance(T_value, dict):  # 还有分支情况
-                return ID3_predict_one(DTree=T_value, row_data=row_data)
+                return predict_one_by_one(DTree=T_value, row_data=row_data)
             else:  # 叶子节点情况
                 return T_value
 
-def ID3_predict(DTree, new_data):
+def predict(DTree, new_data):
     predict_Y = pd.Series([])
 
     # 样本最多的类别
@@ -372,9 +373,9 @@ def ID3_predict(DTree, new_data):
 
     # 逐条样本预测
     for row_index, row_data in new_data.iterrows():
-        pre_y = ID3_predict_one(DTree, row_data)
+        pre_y = predict_one_by_one(DTree, row_data)
         # if pre_y == None:
-        #     pre_y = most_leaf     # 问题已修复，应该不会出现NONE了！【待修改】
+        #     pre_y = most_leaf     # 出现NONE，强制赋值为初始样本样本数最多的类别！【待修改】
         predict_Y[row_index] = pre_y
 
     return predict_Y
@@ -386,13 +387,16 @@ for i in np.arange(len(data.columns)):
     data.iloc[:,i] = data.iloc[:,i].astype('category')
 data = data.drop(['id'],axis=1)
 
+# # 增加连续型变量
+# data['density'] = [0.243, 0.245, 0.343, 0.36, 0.403, 0.437, 0.481, 0.556, 0.593, 0.608, 0.634, 0.639, 0.657, 0.666, 0.697, 0.719, 0.774]
+
 X = data.drop(['haogua'], axis=1)
 y = data['haogua']
 
-clf = DTree(algorithm='ID3', delta=0.001)
+clf = DTree(method='C4.5', delta=0.01)
 clf.fit(X=X, y=y)
 clf.DTree
-y_test = clf.predict(new_data=X_test)
+y_test = clf.predict(new_data=X)
 
 # 2.Kaggle Titanic Data
 # 读取数据
@@ -414,7 +418,7 @@ X_test = train_test.drop(['Survived'], axis=1)
 y_test = train_test['Survived']
 
 # 分类器
-clf = DTree(algorithm='ID3', delta=0.001)
+clf = DTree(method='C4.5', delta=0.001)
 # 训练
 start = time.clock()
 clf.fit(X=X_train, y=y_train)
